605c2e8c81164c00d85eda208c397e60a7b92ee3
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-05-20 13:12:18 +0530

    add new comments to Factorial file

--- DSA_in_Java/Recursion/Factorial_In_Recursion.java
+++ DSA_in_Java/Recursion/Factorial_In_Recursion.java
@@ -1,16 +1,26 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class Factorial_In_Recursion {
     public static void main(String[] args) {
 
-        int n = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number for the Fibonacci series");
+        int n = sc.nextInt();
+
+        // storing the static method to an int variable because the function is returning an int value
         int ans = factorial(n);
         System.out.println(ans);
     }
+
+    // static function for factorial print
     public static int factorial(int n){
 
+        // base case for terminating the recursive call
         if(n == 0) return 1;
 
+        // recursive case for calculating the factorial
         return n * factorial(n-1);
     }
 }
