e76b4ee677dd983372c057be33ed00db4f360aa7
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-05-21 11:31:59 +0530

    update fibonacci logic

--- DSA_in_Java/Recursion/Fibonacci_Recursion.java
+++ DSA_in_Java/Recursion/Fibonacci_Recursion.java
@@ -1,19 +1,28 @@
 package Recursion;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Fibonacci_Recursion {
     public static void main(String[] args) {
 
-        int n = 7;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number for Fibonacci series");
+        int n = sc.nextInt();
 
         for (int i = 0 ; i<=n ; i++){
             System.out.print(fibo(i) + " ");
         }
     }
+
+    // static function for printing Fibonacci series
     public static int fibo(int n){
 
+        // base case for terminating recursive call
         if(n == 0) return 0;
-        if(n == 1 || n == 2) return 1;
+        if(n == 1) return 1;
 
+        // recursive call for calculating the Fibonacci numbers
         return fibo(n-1) + fibo(n-2);
     }
 }
