7084cfa581f26e9dbdc5cf418495889ff74eafce
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-05-22 13:03:27 +0530

    update selecction sort file logic

--- DSA_in_Java/Sorting/Selection_Sort.java
+++ DSA_in_Java/Sorting/Selection_Sort.java
@@ -1,25 +1,50 @@
 package Sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Selection_Sort {
     public static void main(String[] args) {
 
-        int[] arr = {1, 5, 3, 8, 2};
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < arr.length; i++) {
+        System.out.println("Enter the size for an array");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter elements for an array");
+        for(int i = 0 ; i< size ; i++){
+            array[i] = sc.nextInt();
+        }
+        selectionSort(array);
+        System.out.println("Sorted Array: " + Arrays.toString(array));
+    }
+
+
+    public static void selectionSort(int[] array){
+
+        int len = array.length;
+
+        for(int i = 0 ; i < len - 1 ; i++){
+
             int minIndex = i;
 
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
+            for(int j = i + 1 ; j < len; j++)
+            {
+                if(array[j] < array[minIndex]){
+
                     minIndex = j;
+
                 }
+                swap(array , minIndex, i);
             }
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
         }
-        for (int j : arr) {
-            System.out.print(j + " ");
-        }
+    }
+    public static void swap(int[] array , int index1 , int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
 
@@ -27,5 +52,5 @@ public class Selection_Sort {
 Selection Sort:
 • simple sorting technique jo har pass me smallest element ko pick karta hai
 • correct position par place karta hai.
-• Use case: Chhote arrays ya basic logic samajhne ke liye (O(n^2)).
+• Use case: Chote arrays ya basic logic samajhne ke liye (O(n^2)).
 */
