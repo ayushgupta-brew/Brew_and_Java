e1ea5891683e5a6d713a26285e1c2e14fd602306
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-05-28 12:02:17 +0530

    update logic for move zero to end program

--- DSA_in_Java/Arrays_and_2dArrays/One_D_Array/Move_Zeros_toEnd.java
+++ DSA_in_Java/Arrays_and_2dArrays/One_D_Array/Move_Zeros_toEnd.java
@@ -1,26 +1,43 @@
 package Arrays_and_2dArrays.One_D_Array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Move_Zeros_toEnd {
     public static void main(String[] args) {
-        int[] a = {1 , 2 , 0 , 4 , 3 , 0 , 5 , 0};
-        int n = a.length;
-        int j = -1;
-        for(int i = 0 ; i<n ; i++){
-            if(a[i] == 0){
-                j=i;
-                break;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements");
+        for(int i = 0 ; i < size; i++){
+            array[i] = sc.nextInt();
+        }
+
+        moveZeroToEnd(array);
+        System.out.println(Arrays.toString(array));
+
+        sc.close();
+
+    }
+    public static void moveZeroToEnd(int[] array){
+
+        int insertPos = 0;
+
+        for (int i = 0 ; i < array.length ; i++) {
+            if (array[i] != 0) {
+                swap(array , insertPos, i);
+                insertPos++;
             }
         }
-        for(int i = j ; i<n-1 ; i++){
-            if(a[i+1] != 0){
-                int temp = a[i+1];
-                a[i+1] = a[j];
-                a[j] = temp;
-                j++;
-            }
-        }
-        for (int k : a) {
-            System.out.print(k + " ");
-        }
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
