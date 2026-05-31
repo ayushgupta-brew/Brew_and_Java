4b05fe2d346dc0c056a8420c4e7f7ef8d74fcebf
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-05-31 23:10:37 +0530

    add comments

--- DSA_in_Java/Stacks/Reverse_Stack.java
+++ DSA_in_Java/Stacks/Reverse_Stack.java
@@ -7,6 +7,8 @@ public class Reverse_Stack {
     public static void main(String[] args) {
 
         Stack<Integer> st = new Stack<>();
+
+        // Push elements into the stack
         st.push(10);
         st.push(20);
         st.push(30);
@@ -19,12 +21,14 @@ public class Reverse_Stack {
 
     public static void reverseStackUingArrayList(Stack<Integer> st){
 
+        // Stores popped elements in reverse order
         ArrayList<Integer> list= new ArrayList<>();
 
         while(!st.isEmpty()){
             list.add(st.pop());
         }
 
+        // Remove all elements from stack
         for(int i : list){
             st.push(i);
         }
@@ -34,8 +38,10 @@ public class Reverse_Stack {
 
     public static void reverseStackUingStack(Stack<Integer> st){
 
+        // Temporary stack used to reverse the order
         Stack<Integer> temp = new Stack<>();
 
+        // Move all elements to temp stack
         while(!st.isEmpty()){
 //            int top = st.pop();
 //            System.out.print(top + " ");
