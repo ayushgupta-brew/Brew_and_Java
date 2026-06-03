f94c2771ab3ed798d648f12b4a1a0a6bcf58188b
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-03 12:19:39 +0530

    add comments

--- DSA_in_Java/Strings/Length_of_last_word.java
+++ DSA_in_Java/Strings/Length_of_last_word.java
@@ -5,24 +5,34 @@ import java.util.Scanner;
 public class Length_of_last_word {
     static void main(String[] args){
 
+        // Read input from the user
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a string of words");
         String s = sc.nextLine();
 
         int result = lengthOfLastWord(s);
+
+        //  Print the length of the last word
         System.out.println("answer :" + result);
+
+        sc.close();
     }
 
     public static int lengthOfLastWord(String s){
 
+        // Start from the last character of the string
         int tracker = s.length() - 1;
 
+        // Skip trailing spaces
         while(tracker >= 0 && s.charAt(tracker) == ' '){
             tracker--;
         }
 
+        // Stores the length of the last word
         int length=0;
+
+        // Count characters until a space or beginning of string is reached
         while(tracker >= 0 && s.charAt(tracker) != ' '){
             length++;
             tracker--;
