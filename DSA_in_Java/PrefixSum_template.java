0a77e864cb0f7e9111359771fcbfc1da081e491b
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-03 11:39:07 +0530

    add prefix sum template

--- DSA_in_Java/PrefixSum_template.java
+++ DSA_in_Java/PrefixSum_template.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class PrefixSum_template {
+    public static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int[] nums = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        prefixSum(nums);
+    }
+
+    static int[] prefixSum(int[] nums){
+
+        int len = nums.length;
+
+        int[] prefix = new int[len];
+
+        prefix[0] = nums[0];
+
+        for (int i = 0; i < prefix.length; i++) {
+            prefix[i] = prefix[i] + nums[i];
+        }
+        return prefix;
+    }
 }
