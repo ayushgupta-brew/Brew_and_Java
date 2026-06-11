b9bb73b659548d1320fb69fa12289b351a786123
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-11 12:14:25 +0530

    update file logic

--- DSA_in_Java/Arrays_and_2dArrays/One_D_Array/Two_Sum.java
+++ DSA_in_Java/Arrays_and_2dArrays/One_D_Array/Two_Sum.java
@@ -1,38 +1,42 @@
 package Arrays_and_2dArrays.One_D_Array;
-import java.util.Scanner;
-public class Two_Sum {                        // Brute Force O(n^2)
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Two_Sum {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for(int i = 0 ; i< n ; i++){
-            a[i] = sc.nextInt();
-        }
-        int k = sc.nextInt();
-        Calculation1(a , k , n);
-//        Calculation2(a , k , n);
+
+
+        int[] a = {2,6,5,8,11};
+        int target = 14;
+        int[] answer = usingHashMap(a , target);
+        System.out.println(Arrays.toString(answer));
 
     }
-    public static void Calculation1(int[] a , int k , int n){
-        for(int i = 0 ; i<n ; i++){
+    public static int[] usingNestedLoop(int[] a , int target){
+        int n = a.length;
+        for(int i = 0 ; i< n; i++){
             for(int j = i+1 ; j < n ; j++){
-                if(a[i] + a[j] == k){
-                    System.out.print(a[i] + " " + a[j]);
-                    return;
+                if(a[i] + a[j] == target){
+                    return new int[]{i , j};
                 }
             }
         }
+        return new int[]{-1 , -1};
     }
-    public static void Calculation2(int[] a , int k , int n){
-        boolean flag = false;
-        for(int i = 0 ; i<n && !flag ; i++){
-            for(int j = i+1 ; j < n ; j++){
-                if(a[i] + a[j] == k){
-                    System.out.print(a[i] + " " + a[j]);
-                    flag = true;
-                    break;
-                }
+
+    public static int[] usingHashMap(int[] array , int target){
+
+        Map<Integer , Integer> map = new HashMap<>();
+        for(int i = 0 ; i < array.length ; i++){
+            int complement = target - array[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement) , i};
             }
+            map.put(array[i] , i);
         }
+        return new int[]{-1 , -1};
     }
 }
