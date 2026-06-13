03f0fcbe4f26989e034b4f5bf0eb3556b5bc2579
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-13 13:03:16 +0530

    modify the file logic

--- DSA_in_Java/HashMaps/Contains_Duplicate.java
+++ DSA_in_Java/HashMaps/Contains_Duplicate.java
@@ -1,4 +1,39 @@
 package HashMaps;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Contains_Duplicate {
+    static void main(String[] args) {
+
+        int[] nums = {1, 2, 2, 4};
+
+        System.out.println(usingHashSet(nums));
+    }
+
+    public static boolean usingHashMap(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean usingHashSet(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if(!set.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
