5a51b7c72484618c96377d8a6da64825925d62ec
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-16 12:07:17 +0530

    new file add

---
+++ DSA_in_Java/Linked_Lists/LinkedListImpl.java
@@ -0,0 +1,18 @@
+package Linked_Lists;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        data = this.data;
+        next = null;
+    }
+}
+
+class LinkedListOperations{
+    private Node head;
+    private int size;
+
+    
+}
