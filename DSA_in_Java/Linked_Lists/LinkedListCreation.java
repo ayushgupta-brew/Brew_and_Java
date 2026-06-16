a0e25a5f2ed8d3595ac06feef4897987dc34f869
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-16 12:15:43 +0530

    WIP: LinkedList changes

--- DSA_in_Java/Linked_Lists/LinkedListCreation.java
+++ DSA_in_Java/Linked_Lists/LinkedListCreation.java
@@ -3,17 +3,6 @@ package Linked_Lists;
 public class LinkedListCreation {
     public static void main(String[] args) {
 
-        LLoperations list = new LLoperations();
-
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-
-        list.peek();
-        list.deleteUsingData(30);
-    
-        list.print();
-        System.out.println(list.size());
+        Li
     }
 }
