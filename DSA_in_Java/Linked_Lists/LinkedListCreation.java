9229e0ce4cec240854af64dc24652007576cc34d
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-17 12:36:38 +0530

    add LinkedList main class

--- DSA_in_Java/Linked_Lists/LinkedListCreation.java
+++ DSA_in_Java/Linked_Lists/LinkedListCreation.java
@@ -3,6 +3,16 @@ package Linked_Lists;
 public class LinkedListCreation {
     public static void main(String[] args) {
 
-        LinkedListOperations list = new LinkedListOperations();
+        LinkedListOperations list = new LinkedListImpl();
+
+        list.insertAtFront(10);
+        list.insertAtBack(20);
+        list.insertAtBack(30);
+
+        int size = list.getSize();
+        System.out.println("Size of the List: " + size);
+
+        list.printList();
+
     }
 }
