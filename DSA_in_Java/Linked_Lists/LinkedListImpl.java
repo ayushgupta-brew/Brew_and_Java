4c3b9bb7dec7aa5ec1885240e96277546d5a69d3
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-17 12:38:02 +0530

    add Linkedlist implementation file

--- DSA_in_Java/Linked_Lists/LinkedListImpl.java
+++ DSA_in_Java/Linked_Lists/LinkedListImpl.java
@@ -1,4 +1,59 @@
 package Linked_Lists;
 
-public class LinkedListImpl {
+public class LinkedListImpl implements LinkedListOperations {
+
+    private static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private Node head;
+    private int size;
+
+
+    @Override
+    public void insertAtFront(int data) {
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    @Override
+    public void insertAtBack(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            insertAtFront(data);
+            return;
+        }
+
+        Node current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = newNode;
+        size++;
+    }
+
+    @Override
+    public int getSize(){
+        return size;
+    }
+
+    @Override
+    public void printList() {
+        Node current = head;
+
+        while(current != null){
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
 }
