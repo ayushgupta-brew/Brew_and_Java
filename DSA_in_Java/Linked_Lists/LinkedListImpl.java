e8547fe934c5960da4169bbf2ee832902f369b47
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-18 12:40:10 +0530

    implemented new functions logic

--- DSA_in_Java/Linked_Lists/LinkedListImpl.java
+++ DSA_in_Java/Linked_Lists/LinkedListImpl.java
@@ -42,6 +42,78 @@ public class LinkedListImpl implements LinkedListOperations {
     }
 
     @Override
+    public void insertAtPosition(int data, int position) {
+        Node newNode = new Node(data);
+
+        if(position < 0 || position > size){
+            throw new IndexOutOfBoundsException("Invalid Position");
+        }
+        if(position == 0){
+            insertAtFront(data);
+            return;
+        }
+        if(position == size){
+            insertAtBack(data);
+            return;
+        }
+        Node current = head;
+        for(int i = 0 ; i < position - 1 ; i++){
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+    }
+
+    @Override
+    public void deleteAtFront() {
+        if(head == null){
+            throw new IllegalStateException("List is empty");
+        }
+        head = head.next;
+        size--;
+    }
+
+    @Override
+    public void deleteAtBack() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        if(head.next == null){
+            head = null;
+            size--;
+            return;
+        }
+        Node current = head;
+
+        while(current.next.next != null){
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    @Override
+    public void deleteAtPosition(int position) {
+        if (position < 0 || position >= size) {
+            throw new IndexOutOfBoundsException("Invalid position");
+        }
+        if (position == 0) {
+            head = head.next;
+            size--;
+            return;
+        }
+        Node current = head;
+
+        for (int i = 0; i < position - 1; i++) {
+            current = current.next;
+        }
+
+        current.next = current.next.next;
+        size--;
+    }
+
+    @Override
     public int getSize(){
         return size;
     }
