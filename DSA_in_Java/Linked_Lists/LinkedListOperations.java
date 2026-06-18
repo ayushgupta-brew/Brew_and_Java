62eff215addd32b363d34fde251c9d45e2f07399
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-18 12:39:41 +0530

    new functions added

--- DSA_in_Java/Linked_Lists/LinkedListOperations.java
+++ DSA_in_Java/Linked_Lists/LinkedListOperations.java
@@ -7,6 +7,10 @@ public interface LinkedListOperations {
 
     public void insertAtFront(int data);
     public void insertAtBack(int data);
+    public void insertAtPosition(int data , int position);
+    public void deleteAtFront();
+    public void deleteAtBack();
+    public void deleteAtPosition(int position);
     public int getSize();
     public void printList();
 }
