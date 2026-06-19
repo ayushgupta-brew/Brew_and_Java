18a047b2f6e0039e72b6495ba2496f0c4aa8dbf4
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-19 11:58:11 +0530

    test new implemented method

--- DSA_in_Java/Linked_Lists/LinkedListCreation.java
+++ DSA_in_Java/Linked_Lists/LinkedListCreation.java
@@ -8,10 +8,15 @@ public class LinkedListCreation {
         list.insertAtFront(10);
         list.insertAtBack(20);
         list.insertAtBack(30);
+        list.insertAtBack(40);
+        list.insertAtBack(40);
         list.insertAtPosition(15 , 2);
         list.deleteAtPosition(2);
         list.deleteAtFront();
         list.deleteAtBack();
+        boolean answer = list.searchByData(12);
+
+        list.reverseList();
 
         int size = list.getSize();
         System.out.println("Size of the List: " + size);
