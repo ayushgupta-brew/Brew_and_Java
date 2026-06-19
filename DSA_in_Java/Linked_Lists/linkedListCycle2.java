76eaf218a3fd9d1f5f30f3f66adc57af2daafc93
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-06-19 12:30:31 +0530

    add the logic for the new prroblem

--- DSA_in_Java/Linked_Lists/linkedListCycle2.java
+++ DSA_in_Java/Linked_Lists/linkedListCycle2.java
@@ -1,4 +1,37 @@
 package Linked_Lists;
 
 public class linkedListCycle2 {
+    static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        head.next.next.next.next = head.next;
+
+        Node result = detectCycle(head);
+        if(result != null) System.out.println("Cycle starts at node with value: " + result.data);
+        else System.out.println("No cycle found.");
+
+    }
+
+    public static Node detectCycle(Node head){
+        Node fast = head;
+        Node slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
 }
