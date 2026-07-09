c66272efc01a1d36b14a753b83eb18c13b98c2ed
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-09 22:55:00 +0530

    update code logic and add comments

--- Java_concepts/Strings/ToStringExample.java
+++ Java_concepts/Strings/ToStringExample.java
@@ -1,28 +1,73 @@
 package Strings;
 
-// when ever we print the object it by default have .toString() that's why it prints the Hashcode
+/*
+ * Every class in Java automatically inherits the toString() method
+ * from the Object class.
+ *
+ * When we print an object, Java internally calls:
+ *      object.toString();
+ *
+ * If we do not override it, the default implementation prints:
+ *      ClassName@HexadecimalHashCode
+ *
+ * By overriding toString(), we can control how the object is displayed.
+ */
 
-// we can override the toString method by implementing a new method of same name with our method definition .
-
-// we can override any method definition by replacing it with our definition
-
-class laptop{
+// -------------------- Without overriding toString() --------------------
+class Laptop1 {
     String name;
     int number;
 
-    public String toString(){  // we can modify the toString method
-        return "Hey";
+    // Uses Object class's default toString() method
+}
+
+// -------------------- With overriding toString() --------------------
+class Laptop2 {
+    String name;
+    int number;
+
+    // Custom implementation of toString()
+    // This method will be called automatically whenever the object is printed.
+    @Override
+    public String toString() {
+        return "Laptop{name='" + name + "', number=" + number + "}";
     }
 }
 
-public class Hashcode {
+public class ToStringExample {
+
     public static void main(String[] args) {
 
-        laptop obj = new laptop();
-        obj.name = "Ayush";
-        obj.number = 10;
+        // Creating an object of Laptop1
+        Laptop1 l1 = new Laptop1();
+        l1.name = "Ayush";
+        l1.number = 10;
 
-        System.out.println(obj);  // Strings.laptop@6acbcfc0 (output)
-        System.out.println(obj.toString());  // Hey (output)
+        // Creating an object of Laptop2
+        Laptop2 l2 = new Laptop2();
+        l2.name = "Ayush";
+        l2.number = 10;
+
+        System.out.println("Without overriding toString():");
+
+        // Internally Java executes:
+        // System.out.println(l1.toString());
+        //
+        // Since Laptop1 does not override toString(),
+        // Object class's implementation is used.
+        // Output format:
+        // ClassName@HexadecimalHashCode
+        System.out.println(l1);
+
+        System.out.println();
+
+        System.out.println("After overriding toString():");
+
+        // Internally Java executes:
+        // System.out.println(l2.toString());
+        //
+        // Since Laptop2 overrides toString(),
+        // our custom string representation is printed.
+        System.out.println(l2);
     }
 }
