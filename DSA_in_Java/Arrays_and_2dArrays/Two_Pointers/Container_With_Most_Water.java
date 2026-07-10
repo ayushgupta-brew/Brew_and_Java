19ba55aa9918b6b80ddb0b5920a6936ac02c4d80
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-10 10:00:47 +0530

    add code logic to the file

--- DSA_in_Java/Arrays_and_2dArrays/Two_Pointers/Container_With_Most_Water.java
+++ DSA_in_Java/Arrays_and_2dArrays/Two_Pointers/Container_With_Most_Water.java
@@ -1,4 +1,27 @@
 package Arrays_and_2dArrays.Two_Pointers;
 
 public class Container_With_Most_Water {
+    static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println(maxArea(height));
+    }
+    public static int maxArea(int[] height){
+        int maxAreaLength = Integer.MIN_VALUE;
+        int left = 0 , right = height.length - 1;
+
+        while(left < right){
+
+            int minHeight = Math.min(height[left] , height[right]);
+            int width = right - left;
+
+            int area = minHeight * width;
+
+            maxAreaLength = Math.max(maxAreaLength , area);
+
+            if(height[left] <= height[right]) left++;
+            else right--;
+        }
+        return maxAreaLength;
+    }
 }
